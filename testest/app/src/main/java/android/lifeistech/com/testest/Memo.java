35e6d36b3ef91b41f41cfb0f86ea9cc4cf4ecf4b
minishimura <nassyi1217@gmail.com>
2019-02-23 04:01:01 +0900

    0224

---
+++ testest/app/src/main/java/android/lifeistech/com/testest/Memo.java
@@ -0,0 +1,9 @@
+package android.lifeistech.com.testest;
+
+import io.realm.RealmObject;
+
+public class Memo extends RealmObject {
+    public String title;
+    public String content;
+    public String updateDate;
+}
